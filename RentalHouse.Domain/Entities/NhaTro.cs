a9dd1d273fff0bd80e397494a25dcc390da24185
Namne2k3 <nhpn2003@gmail.com>
2025-02-14 13:28:40 +0700

    Add Domain Layer

---
+++ RentalHouse.Domain/Entities/NhaTro.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentalHouse.Domain.Entities
+{
+    public class NhaTro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(500)")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "nvarchar(255)")]
+        public string Address { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? DescriptionHtml { get; set; }
+
+        [Column(TypeName = "nvarchar(500)")]
+        public string? Url { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Price { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? PriceExt { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Area { get; set; }
+
+        public DateTime? PostedDate { get; set; }
+        public DateTime? ExpiredDate { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Type { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Code { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? ElectricityPrice { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string? InternetPrice { get; set; }
+
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Utilities { get; set; }
+
+        [Column(TypeName = "nvarchar(1000)")]
+        public string? Furniture { get; set; }
+
+        public float? Latitude { get; set; }
+        public float? Longitude { get; set; }
+
+        public float? PriceBil { get; set; }
+        public float? PriceMil { get; set; }
+        public float? PriceVnd { get; set; }
+        public float? AreaM2 { get; set; }
+        public float? PricePerM2 { get; set; }
+
+        // Quan hệ 1-N với NhaTroImages
+        public List<NhaTroImage> Images { get; set; } = new();
+    }
+}
---
+++ RentalHouse.Domain/Entities/NhaTroImage.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentalHouse.Domain.Entities
+{
+    public class NhaTroImage
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string ImageUrl { get; set; } = string.Empty;
+        [ForeignKey("NhaTro")]
+        public int NhaTroID { get; set; }
+        public required NhaTro NhaTro { get; set; }
+    }
+}
